20d8c5fb824190701e49696c58c97eea865ea7d0
Andyrexic <dev.andrewpeel@gmail.com>
2017-09-06 21:18:59 -0700

    Added outliner script

--- __init__.py
+++ __init__.py
@@ -3,8 +3,10 @@ import bpy
 from . import object_properties_panel
 from . import remove_blender_ui
 from . import room_designer
+from . import ui_layer_manager
 
 def register():
 	object_properties_panel.register()
 	remove_blender_ui.register()
 	room_designer.register()
+	ui_layer_manager.register()
